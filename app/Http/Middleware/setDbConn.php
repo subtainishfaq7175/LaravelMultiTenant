a61dcce2c4c26e4677b7adb6cfe1996dad9d38cb
Danyal Sheikh <danyalsheikh.vbase@gmail.com>
2017-07-18 23:02:33 +0500

    code commenting

--- app/Http/Middleware/setDbConn.php
+++ app/Http/Middleware/setDbConn.php
@@ -18,10 +18,10 @@ class setDbConn
     {
         $dbName = isset(Session::get('company')->db_name) ? Session::get('company')->db_name : false;
         if ($dbName) {
-            \Config::set('database.connections.mysql.database', $dbName);
+            \Config::set('database.connections.mysql.database', $dbName);   // Set main database to company database
             return $next($request);
         } else {
-            \Auth::logout();
+            \Auth::logout();    // Logout any logged in user and redirect to /domain page
             return redirect('/domain')->with('message', 'please login again to continue..!');
         }
     }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,38 +15,46 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Auth Routes
 Route::auth();
 
+//Home Route
 Route::get('/home', [
     'as' => 'home',
     'uses' => 'HomeController@index'
 ]);
 
+//Create New Company Route
 Route::get('/company', [
     'as' => 'company',
     'uses' => 'CompanyController@index'
 ]);
 
+//View Company Detail Route
 Route::get('/companydetail/{id}', [
     'as' => 'companydetail',
     'uses' => 'CompanyController@show'
 ]);
 
+//Edit Company Info Route
 Route::get('/companyinfo/{id}', [
     'as' => 'companyinfo',
     'uses' => 'CompanyController@edit'
 ]);
 
+//Check Sub-domain Route
 Route::get('/domain', [
     'as' => 'domain',
     'uses' => 'DomainChecker@index'
 ]);
 
+//View Company User Information Route
 Route::get('/user/{id}', [
     'as' => 'user',
     'uses' => 'CompanyController@adduser'
 ]);
 
+//Company User Login Route
 Route::get('/companylogin', [
     'as' => 'companylogin',
     'uses' => 'DomainChecker@companyUserLogin'
@@ -54,48 +62,57 @@ Route::get('/companylogin', [
 
 // Post Routes
 
+//Post Route to register new company
 Route::post('/registerCompany', [
     'as' => 'registerCompany',
     'uses' => 'CompanyController@store'
 ]);
 
+//Post Route to update company info
 Route::post('/updateCompany', [
     'as' => 'updateCompany',
     'uses' => 'CompanyController@update'
 ]);
 
+//Post Route to create company user
 Route::post('/companyUser', [
     'as' => 'companyUser',
     'uses' => 'CompanyUsersController@store'
 ]);
 
+//Post Route to delete company
 Route::post('/companyDelete', [
     'as' => 'companyDelete',
     'uses' => 'CompanyController@companyDelete'
 ]);
 
+//Post Route to delete company user
 Route::post('/companyUserDelete', [
     'as' => 'companyUserDelete',
     'uses' => 'CompanyUsersController@companyUserDelete'
 ]);
 
+//Post Route to check sub-domain
 Route::post('/checkDomain', [
     'as' => 'checkDomain',
     'uses' => 'DomainChecker@domainChecker'
 ]);
 
+//Post Route to login company user
 Route::post('/companylogin', [
     'as' => 'postCompanylogin',
     'uses' => 'DomainChecker@postCompanyLogin'
 ]);
 
-
+//Company User after login routes group
 Route::group(['middleware' => 'setDbConn'], function (){
+    //Company Home Route
     Route::get('/companyHome', [
         'as' => 'companyHome',
         'uses' => 'DomainChecker@companyHome'
     ]);
 
+    //Save Company user Log Route
     Route::post('/saveLog', [
         'as' => 'saveLog',
         'uses' => 'CompanyUsersController@saveLog'
